12319301994291daff148b9af5cd68fca1ac0c9b
Alireza S. Mahani <alireza.mahani@sentrana.com>
2015-01-01 00:00:00 +0000

    version 0.7.1

--- R/expanders.R
+++ R/expanders.R
@@ -1,48 +1,52 @@
-regfac.expand.1par <- function(beta, X, y, fbase) {
+regfac.expand.1par <- function(beta, X, y, fbase1, fgh=2, ...) {
   # obtain base distribution derivatives
-  ret <- fbase(X%*%beta,y)
+  ret <- fbase1(X%*%beta,y,fgh, ...)
   # expand base derivatives
   f <- sum(ret$f)
+  if (fgh==0) return (f)
   g <- t(X)%*%ret$g
+  if (fgh==1) return (list(f=f, g=g))
 	xtw <- 0*X
 	for (k in 1:ncol(X)) xtw[,k] <- X[,k]*ret$h # TODO: convert for loop to sapply
 	h <- t(xtw)%*%X
-  
   return (list(f=f, g=g, h=h))
 }
 
-regfac.expand.2par <- function(coeff, X, Z=matrix(1.0, nrow=nrow(X), ncol=1), y, fbase, block.diag=FALSE) {
+regfac.expand.2par <- function(coeff, X, Z=matrix(1.0, nrow=nrow(X), ncol=1), y, fbase2, fgh=2, block.diag=FALSE, ...) {
   # extracting coefficients of X and Z
-  beta <- coeff[1:ncol(X)]
-  gamma <- coeff[(ncol(X)+1):(ncol(X)+ncol(Z))]
+  K1 <- ncol(X); K2 <- ncol(Z)
+  beta <- coeff[1:K1]
+  gamma <- coeff[K1 + 1:K2]
   
   # obtain base distribution derivatives
-  ret <- fbase(X%*%beta,Z%*%gamma,y)
+  ret <- fbase2(X%*%beta, Z%*%gamma, y, fgh, ...)
 
   # expand base derivatives; TODO: vectorize for loops
   # function
   f <- sum(ret$f)
+  if (fgh==0) return (f)
   # first derivative
   g <- c(t(X)%*%ret$g[,1],t(Z)%*%ret$g[,2])
+  if (fgh==1) return (list(f=f, g=g))
   # second derivative
-	h <- array(0, dim=c(ncol(X)+ncol(Z),ncol(X)+ncol(Z)))
+	h <- array(0, dim=c(K1+K2, K1+K2))
 	# XX block
 	xtw <- 0*X
-	for (k in 1:ncol(X)) xtw[,k] <- X[,k]*ret$h[,1]
-	h[1:ncol(X),1:ncol(X)] <- t(xtw)%*%X
+	for (k in 1:K1) xtw[,k] <- X[,k]*ret$h[,1]
+	h[1:K1, 1:K1] <- t(xtw)%*%X
 	# ZZ block
 	ztw <- 0*Z
-	for (k in 1:ncol(Z)) ztw[,k] <- Z[,k]*ret$h[,2]
-	h[(ncol(X)+1):(ncol(X)+ncol(Z)),(ncol(X)+1):(ncol(X)+ncol(Z))] <- t(ztw)%*%Z
+	for (k in 1:K2) ztw[,k] <- Z[,k]*ret$h[,2]
+	h[K1 + 1:K2, K1 + 1:K2] <- t(ztw)%*%Z
 	# XZ and ZX blocks
 	if (!block.diag) {
 	  ztw2 <- 0*Z
-	  for (k in 1:ncol(Z)) ztw2[,k] <- Z[,k]*ret$h[,3]
-	  h[(ncol(X)+1):(ncol(X)+ncol(Z)),1:ncol(X)] <- t(ztw2)%*%X
-	  h[1:ncol(X),(ncol(X)+1):(ncol(X)+ncol(Z))] <- t(h[(ncol(X)+1):(ncol(X)+ncol(Z)),1:ncol(X)])
+	  for (k in 1:K2) ztw2[,k] <- Z[,k]*ret$h[,3]
+	  h[K1 + 1:K2, 1:K1] <- t(ztw2)%*%X
+	  h[1:K1, K1 + 1:K2] <- t(h[K1 + 1:K2, 1:K1])
 	}
 	
-  return (list(f=f,g=g,h=h))
+  return (list(f=f, g=g, h=h))
 }
 
 
--- R/fbase.1par.R
+++ R/fbase.1par.R
@@ -1,31 +1,82 @@
-fbase.bern.ilogit <- function(u,y) {
+# log-likelihood for Binomial distribution (assuming number of trials, n, is given)
+# logit link function
+fbase1.binomial.logit <- function(u, y, fgh=2, n=1) {
   eu <- exp(u)
-  f <- -log(1+1/eu)-(1-y)*u
-  g <- 1/(1+eu)-(1-y)
-  h <- -eu/(1+eu)^2
+  f <- -n*log(1+1/eu) - (n-y)*u # ignoring C(u) terms such as log-factorials
+  if (fgh==0) return (list(f=f))
+  g <- y-n/(1+1/eu)
+  if (fgh==1) return (list(f=f, g=g))
+  h <- -n*eu/(1+eu)^2
+  return (list(f=f,g=g,h=h))
+}
+# probit link function
+fbase1.binomial.probit <- function(u, y, fgh=2, n=1) {
+  phi <- dnorm(u)
+  Phi <- pnorm(u)
+  f <- y*log(Phi) + (n-y)*log(1-Phi)
+  if (fgh==0) return (list(f=f))
+  g <- phi*(y-n*Phi)/(Phi*(1-Phi))
+  if (fgh==1) return (list(f=f, g=g))
+  h <- y*(u*phi/Phi - (phi/Phi)^2) - (n-y)*(u*phi/(1-Phi) + (phi/(1-Phi))^2)
+  return (list(f=f,g=g,h=h))
+}
+# cauchit link function
+fbase1.binomial.cauchit <- function(u, y, fgh=2, n=1) {
+  phi <- dcauchy(u)
+  Phi <- pcauchy(u)
+  f <- y*log(Phi) + (n-y)*log(1-Phi)
+  if (fgh==0) return (list(f=f))
+  g <- y*(phi/Phi) - (n-y)*(phi/(1-Phi))
+  if (fgh==1) return (list(f=f, g=g))
+  h <- y*(u*phi/Phi - (phi/Phi)^2) - (n-y)*(u*phi/(1-Phi) + (phi/(1-Phi))^2)
+  return (list(f=f,g=g,h=h))
+}
+# cloglog link function
+fbase1.binomial.cloglog <- function(u, y, fgh=2, n=1) {
+  eu <- exp(u)
+  eeu <- exp(-eu)
+  f <- y*log(1-eeu) - (n-y)*eu
+  if (fgh==0) return (list(f=f))
+  g <- y*(eu/(1-eeu)) - n*eu
+  if (fgh==1) return (list(f=f, g=g))
+  h <- eu*(y*(1-eeu-eu*eeu)/(1-eeu)^2 - n)
   return (list(f=f,g=g,h=h))
 }
 
-fbase.pois.exp <- function(u,y) {
+# log-likelihood for Poisson distribution (count response)
+# log link function
+fbase1.poisson.log <- function(u,y,fgh=2) {
   eu <- exp(u)
-  f <- y*u-eu - lfactorial(y)
+  f <- y*u-eu # ignoring additive log-factorial term since it is independent of u
+  if (fgh==0) return (list(f=f))
   g <- y-eu
+  if (fgh==1) return (list(f=f, g=g))
   h <- -eu
   return (list(f=f,g=g,h=h))
 }
 
-fbase.exp.exp <- function(u,y) {
-  eu <- exp(u)
-  f <- u-y*eu
-  g <- 1-y*eu
-  h <- -y*eu
+# log-likelihood for exponential distribution (positive response)
+# log link function
+fbase1.exponential.log <- function(u,y,fgh=2) {
+  emu <- exp(-u)
+  f <- -u-y*emu
+  if (fgh==0) return (list(f=f))
+  g <- -1+y*emu
+  if (fgh==1) return (list(f=f, g=g))
+  h <- -y*emu
   return (list(f=f,g=g,h=h))
 }
 
-fbase.geo.ilogit <- function(u,y) {
+# log-likelihood for geometric distribution (?? response)
+# logit link function
+# (technically, the link function applies to inverse of mean, which equals p)
+fbase1.geometric.logit <- function(u,y,fgh=2) {
   eu <- exp(u)
   f <- -(y*u+(1+y)*log(1+1/eu))
+  if (fgh==0) return (list(f=f))
   g <- -y+(1+y)/(1+eu)
+  if (fgh==1) return (list(f=f, g=g))
   h <- -(1+y)*eu/(1+eu)^2
   return (list(f=f,g=g,h=h))
 }
+
